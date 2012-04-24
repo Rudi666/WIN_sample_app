e843176fd22599da6bc3575a27546426ef06199e
Joe Karas <rudi4@tampabay.rr.com>
2012-04-23 21:36:41 -0400

    Finish layout and routes. Need to finish tests

--- Gemfile
+++ Gemfile
@@ -1,11 +1,12 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.1'
-
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 #gem 'sqlite3'
+
+gem 'bootstrap-sass', '2.0.0'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new"
 
-  get "static_pages/help"
+  root to: 'static_pages#home'
   
-  get "static_pages/about"
-
+  match '/signup',  to: 'users#new'
+  
+  # get "static_pages/home"
+  # get "static_pages/help"
+  match '/help', to: 'static_pages#help'
+  # get "static_pages/about"
+  match '/about',   to: 'static_pages#about'
+  # get "static_pages/contact"
+  match '/about',   to: 'static_pages#about'
+  match '/contact',   to: 'static_pages#contact'
+  
+  # get "users/new"
+  
+ end
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -61,4 +74,4 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
+
